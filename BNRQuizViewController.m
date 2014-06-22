0fac2409bcaebb433ba26bb3d255deb27c5a007a
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-22 22:01:49 +0700

    Create connection between ViewController and ViewObject

--- BNRQuizViewController.m
+++ BNRQuizViewController.m
@@ -10,8 +10,45 @@
 
 @interface BNRQuizViewController ()
 
+@property (nonatomic) int currentQuestionIndex;
+@property (nonatomic, copy) NSArray *questions;
+@property (nonatomic, copy) NSArray *answers;
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation BNRQuizViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        // Create two arrays filled with questions and answers
+        // and make the pointers point to them
+        self.questions = @[@"From what is cognac made?",
+                           @"What is 7+7?",
+                           @"What is the capital of Vermont?"];
+        self.answers = @[@"Grapes",
+                         @"14",
+                         @"Montpelier"];
+    }
+    
+    // Return the address of the new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender
+{
+
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    
+}
 
 @end
